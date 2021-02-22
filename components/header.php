6db6c7ff97dd1ee0c1d026c6210d669c69a6df3b
ThGDev <thida.dev@gmail.com>
2021-02-22 19:27:19 +0100

    1st commit

---
+++ components/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="/assets/css/style.css" rel="stylesheet" />
+        <link href="/assets/css/header.css" rel="stylesheet" />
+        <link href="/assets/css/footer.css" rel="stylesheet" />
+        <title>Projet en carton</title>
+    </head>
+    <body>
+        <header>
+            <div id="logos">
+                <img id="logoMichelin" src="/assets/img/Michelin-Logo.png" alt="logo michelin">
+                <img id="logoEbay" src="/assets/img/EBay_logo.png" alt="logo ebay">
+            </div>
+            <div id="logoAnniv">
+                <img src="/assets/img/Logo-120-ans_rond.png" alt="logo anniversaire 120ans michelin">
+            </div>
+            <h1>Les enchères solidaires !</h1>
+            <nav>
+                <ul>
+                    <li><a href="/home">accueil</a></li>
+                    <li><a href="/presentation">présentation</a></li>
+                    <li><a href="/inscription">inscription</a></li>
+                </ul>
+            </nav>
+        </header>
